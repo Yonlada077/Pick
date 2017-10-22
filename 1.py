2809ab921306992076d4bdd9632854db9f7662c3
Punmanat <60070053@kmitl.ac.th>
2017-10-22 19:22:30 +0700

    Fixing pick

--- 1.py
+++ 1.py
@@ -4,10 +4,22 @@ def main():
     """print Yes or No and print Value"""
     text = json.loads(input())
     text2 = input()
-    for text3 in text.keys():
-        if text3 == text2:
+    # เพิ่มตัวแปรcount เอาไว้เก็บค่า print
+    count_print = 0
+    for key in text.keys():
+    	# เปลี่ยนชื่อตัวแปรให้เข้าใจง่ายๆหน่อยokนะ
+        if key == text2:
             print("Yes")
+            # เพิ่มบรรทัดนี้เข้าไปให้นะ เราต้องใช้ dict[key] มันจะreturn values ออกมาให
+            print(text[key])
+            count_print += 1
             break
-    if text3 != text2:
-        print("No")
+
+    # กรณีนี้ตัวแปรkey อยู่นอกloopเราเอามาใช้ไม่ได้นะ
+    # if key != text2:
+    #     print("No")
+
+    # ถ้าไม่มีปริ้นkeyเลยก็จะปิ้น No ออกมาให้
+    if count_print == 0:
+        print('No')
 main()
